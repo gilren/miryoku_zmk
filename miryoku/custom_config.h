f891205e5ecb1acd659a27447c1ccb655ba18c30
gilren <gillet.ren@gmail.com>
2024-04-22 20:46:50 +0200

    test

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,20 +11,20 @@
 #define MIRYOKU_CLIPBOARD_WIN
 // #define MIRYOKU_LAYERS_DEFAULT
 
-#if defined (MIRYOKU_KEYBOARD_CORNE)
+// #if defined (MIRYOKU_KEYBOARD_CORNE)
 
-#define XXX &none
+// #define XXX &none
 
-#define MIRYOKU_LAYERMAPPING_BASE( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-&kp HASH      K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp DOLLAR \
-&kp AT_SIGN   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp CARET \
-&kp TAB       K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp u_to_U_GAME \
-                        K32  K33  K34       K35  K36  K37
+// #define MIRYOKU_LAYERMAPPING_BASE( \
+//      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+//      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+//      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+//      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+// ) \
+// &kp HASH      K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp DOLLAR \
+// &kp AT_SIGN   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp CARET \
+// &kp TAB       K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp u_to_U_GAME \
+//                         K32  K33  K34       K35  K36  K37
 
 
 // #define MIRYOKU_LAYERMAPPING_EXTRA( \
@@ -135,7 +135,7 @@
 //                      K32  K33  K34       K35  K36  K37
 
 
-#endif
+// #endif
 
 
 
