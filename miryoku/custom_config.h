d75cd84b4d01a167bfbf1277d1b35eb79381e205
gilren <gillet.ren@gmail.com>
2024-04-22 20:28:43 +0200

    update

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -15,16 +15,16 @@
 
 #define XXX &none
 
-#define MIRYOKU_LAYERMAPPING_BASE( \
-     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-) \
-&kp HASH      K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp DOLLAR \
-&kp AT_SIGN   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp CARET \
-&kp TAB       K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp u_to_U_GAME \
-                        K32  K33  K34       K35  K36  K37
+// #define MIRYOKU_LAYERMAPPING_BASE( \
+//      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+//      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+//      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+//      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+// ) \
+// &kp HASH      K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp DOLLAR \
+// &kp AT_SIGN   K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp CARET \
+// &kp TAB       K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp u_to_U_GAME \
+//                         K32  K33  K34       K35  K36  K37
 
 
 // #define MIRYOKU_LAYERMAPPING_EXTRA( \
@@ -170,27 +170,27 @@ U_NP,                  U_NP,              U_LT(U_MEDIA, ESC),  U_LT(U_NAV, SPACE
 // U_NP,              U_NP,              OP_LPRN,           OP_RPRN,          OP_UNDS,           U_NA,              U_NA,              U_NA,                U_NP,               U_NP
 
 
-#define MIRYOKU_LAYER_GAME \
-&kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
-&kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
-&kp LCTRL,         &kp Z,             &kp X,             &kp C,             U_MT(N4, G),       U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
-U_NP,              U_NP,              U_LT(U_GAME, ESC), &kp LCTRL,         &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
+// #define MIRYOKU_LAYER_GAME \
+// &kp TAB,           &kp Q,             &kp W,             &kp E,             &kp R,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
+// &kp LSHFT,         &kp A,             &kp S,             &kp D,             &kp F,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
+// &kp LCTRL,         &kp Z,             &kp X,             &kp C,             U_MT(N4, G),       U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
+// U_NP,              U_NP,              U_LT(U_GAME, ESC), &kp LCTRL,         &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
 
 
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(GAME,   "Game")
+// #define MIRYOKU_LAYER_LIST \
+// MIRYOKU_X(BASE,   "Base") \
+// MIRYOKU_X(EXTRA,  "Extra") \
+// MIRYOKU_X(TAP,    "Tap") \
+// MIRYOKU_X(BUTTON, "Button") \
+// MIRYOKU_X(NAV,    "Nav") \
+// MIRYOKU_X(MOUSE,  "Mouse") \
+// MIRYOKU_X(MEDIA,  "Media") \
+// MIRYOKU_X(NUM,    "Num") \
+// MIRYOKU_X(SYM,    "Sym") \
+// MIRYOKU_X(FUN,    "Fun") \
+// MIRYOKU_X(GAME,   "Game")
 
-#define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+// #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
 
 #define U_BASE   0
 #define U_EXTRA  1
@@ -202,4 +202,4 @@ MIRYOKU_X(GAME,   "Game")
 #define U_NUM    7
 #define U_SYM    8
 #define U_FUN    9
-#define U_GAME   10
+// #define U_GAME   10
